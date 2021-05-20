339ab220f73cb54a65e4dcae7dc675fc92b2b89c
DidacClaria <dclariah99@gmail.com>
2021-05-20 12:02:05 +0200

    GET notices

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {NoticesList} from './components/NoticesList.js'
+
+export function App(){
+  return(<NoticesList />);
+}
---
+++ src/components/Notice.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Notice extends React.Component {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
