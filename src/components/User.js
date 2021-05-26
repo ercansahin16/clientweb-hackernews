24b406b0cee9fba4e35820571729100daed58b57
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2021-05-27 00:55:11 +0200

    Change user info

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native-web';
+import {Link} from "react-router-dom";
+import '../stylesheets/form.css';
 
 export class User extends React.Component {
 
-  state = {
-
-    comments: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
       loading: true,
       user: {
         id: 1,
@@ -18,6 +21,10 @@ export class User extends React.Component {
         updated_at: "",
         url: ""
       }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount(){
@@ -29,8 +36,69 @@ export class User extends React.Component {
     console.log(data);
   }
 
+  handleChange(event) {
+    const name = event.target.name;
+    console.log(event.target.value);
+    let newUser = this.state.user;
+    if (name == "username") newUser.username = event.target.value;
+    else if (name == "about") newUser.about = event.target.value;
+    this.setState({user: newUser});
+    console.log(this.state.user.username);
+  }
+
+  handleSubmit(event) {
+    const requestOptions = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json', 'X-API-Key': 'p3ggo4igayia', 'Accept': 'application/json' },
+        body: JSON.stringify({user: { username: this.state.user.username, about: this.state.user.about }})
+    };
+    fetch(`https://project-asw.herokuapp.com/users/${this.state.user.id}`, requestOptions)
+        .then(async response => {
+            const isJson = response.headers.get('content-type').includes('application/json');
+            const data = isJson && await response.json();
+
+            console.log(response);
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+        })
+        .catch(error => {
+            this.setState({ errorMessage: error.toString() });
+        });
+        event.preventDefault();
+  }
+
   render() {
-    return (
+
+    if (this.state.user.id == 1) {
+      return (
+          <ul style={{backgroundColor: '#dfeff1'}}>
+              <View style={styles.paddings}>
+                  <h1 style={{fontFamily: 'Verdana, Geneva, sans-serif', fontSize: 20}}>{this.state.user.username}</h1>
+                  <Text> {this.state.user.about} </Text>
+                  <form onSubmit={this.handleSubmit}>
+                  <div class="row">
+                  <input type="text" name="username" value={this.state.user.username} onChange={this.handleChange} placeholder="Write your username..." />
+                  </div>
+                  <div class="row">
+                    <textarea name="about" value={this.state.user.about} onChange={this.handleChange} placeholder="Write something about you..."></textarea>
+                    <input type="submit" value="Submit" />
+                  </div>
+                  </form>
+                  <View style={styles.contentView}>
+                      <Text style={styles.subtext}>Email: {this.state.user.email} | Created at: {this.state.user.created_at}</Text>
+                      <br></br>
+                      <Link to={{pathname:`/threads/${this.state.user.id}`}}> <Text  style={styles.subtext}>comments</Text></Link>
+                  </View>
+              </View>
+          </ul>
+      )}
+    else {
+      return (
         <ul style={{backgroundColor: '#dfeff1'}}>
             <View style={styles.paddings}>
                 <h1 style={{fontFamily: 'Verdana, Geneva, sans-serif', fontSize: 20}}>{this.state.user.username}</h1>
@@ -38,10 +106,11 @@ export class User extends React.Component {
                 <View style={styles.contentView}>
                     <Text style={styles.subtext}>Email: {this.state.user.email} | Created at: {this.state.user.created_at}</Text>
                     <br></br>
+                    <Link to={{pathname:`/threads/${this.state.user.id}`}}> <Text  style={styles.subtext}>comments</Text></Link>
                 </View>
             </View>
         </ul>
-    )
+    )}
   }
 }
 
